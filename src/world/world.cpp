c64fd3d8e0bdc02aa252cafe2065cb0223ba0ba6
mattvchandler <tardarsauce@gmail.com>
2015-07-07 13:52:36 -0800

    enable sf::Listener up vector
    SFML 2.3 now in debian testing

--- src/world/world.cpp
+++ src/world/world.cpp
@@ -812,10 +812,10 @@ void World::main_loop()
         // set audio listener
         glm::vec3 cam_pos = _cam->pos();
         glm::vec3 cam_forward = _cam->forward();
-        // glm::vec3 cam_up = _cam->up();
+        glm::vec3 cam_up = _cam->up();
         sf::Listener::setPosition(cam_pos.x, cam_pos.y, cam_pos.z);
         sf::Listener::setDirection(cam_forward.x, cam_forward.y, cam_forward.z);
-        // sf::Listener::setUpVector(cam_up.x, cam_up.y, cam_up.z); // TODO: available in SFML 2.2
+        sf::Listener::setUpVector(cam_up.x, cam_up.y, cam_up.z);
 
         // TODO should we make more threads for input, physics, messages, etc?
         // TODO: AI
